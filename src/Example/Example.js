525da609a21c4729781b72e48a7a4cc8fe5186c6
HsienW <hsien.w.wei@gmail.com>
2018-11-19 15:25:10 +0800

    update Example Effect

--- src/Example/Example.js
+++ src/Example/Example.js
@@ -1,4 +1,5 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
+import './Example.css';
 
 class ExampleA extends Component {
 
@@ -7,7 +8,7 @@ class ExampleA extends Component {
     };
 
     update = () => {
-        this.setState({count: this.state.count + 1});
+        this.setState({count: this.state.count + 1})
     };
 
     render() {
@@ -23,13 +24,104 @@ class ExampleA extends Component {
 
 const ExampleB = () => {
 
-    const [count, setCount] = useState(0);
+    const [count, update] = useState(0);
+    // const count = useState(0)[0]; 把陣列展開 第1個就是當前的 count
+    // const update = useState(0)[1]; 第2個是 update
+
+    // state = {
+    //     count: 0
+    // };
+    //
+    // const update = (count) => {
+    //     this.setState({count: count}) // 相當於把傳入的 count 做 setState
+    // }
 
     return (
         <div>
             ExampleB
             <div>{count}</div>
-            <button onClick={() => setCount(count + 1)}>Count</button>
+            <button onClick={() => update(count + 1)}>Count</button>
+        </div>
+    );
+};
+
+class ExampleC extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            total: 100,
+            done: 90,
+            undone: 0,
+            showDoneNumber: true,
+            showUndoneNumber: true,
+        };
+        this.barDone = React.createRef();
+        this.doneNumber = React.createRef();
+        this.barUndone = React.createRef();
+        this.undoneNumber = React.createRef();
+    }
+
+
+    componentDidMount() {
+        this.setState({
+            undone: this.state.total - this.state.done,
+            showDoneNumber: true,
+            showUndoneNumber: true,
+        }, () => {
+            if (this.doneNumber.current.offsetWidth >= this.barDone.current.offsetWidth) {
+                this.setState({showDoneNumber: false});
+            }
+            if (this.undoneNumber.current.offsetWidth >= this.barUndone.current.offsetWidth) {
+                this.setState({showUndoneNumber: false});
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="progress-bar">
+                <div className="bar-done" style={{width: this.state.done}} ref={this.barDone}>
+                    <div className={this.state.showDoneNumber ? '' : 'shadow-number'} ref={this.doneNumber}>
+                        {this.state.done}
+                    </div>
+                </div>
+                <div className="bar-undone" style={{width: this.state.undone}} ref={this.barUndone}>
+                    <div className={this.state.showUndoneNumber ? '' : 'shadow-number'} ref={this.undoneNumber}>
+                        {this.state.undone}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+// componentDidMount
+
+const ExampleD = () => {
+
+    const [total, update] = useState(100);
+    const [done] = useState(90);
+    const [undone] = useState(0);
+    const [showDoneNumber] = useState(true);
+    const [showUndoneNumber] = useState(true);
+
+    useEffect(() => {
+
+    });
+
+    return (
+        <div className="progress-bar">
+            <div className="bar-done" style={{width: this.state.done}} ref={this.barDone}>
+                <div className={this.state.showDoneNumber ? '' : 'shadow-number'} ref={this.doneNumber}>
+                    {this.state.done}
+                </div>
+            </div>
+            <div className="bar-undone" style={{width: this.state.undone}} ref={this.barUndone}>
+                <div className={this.state.showUndoneNumber ? '' : 'shadow-number'} ref={this.undoneNumber}>
+                    {this.state.undone}
+                </div>
+            </div>
         </div>
     );
 };
@@ -37,5 +129,7 @@ const ExampleB = () => {
 
 export {
     ExampleA,
-    ExampleB
+    ExampleB,
+    ExampleC,
+    ExampleD
 };
